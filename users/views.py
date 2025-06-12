8e4b42156882189ba208bd4b5a3a01a1000434ca
Rojalin Mohapatra <project.rojalinmohapatra@gmail.com>
2025-06-12 21:36:29 +0530

    Co-authored-by: Sarbeswar Bhol <sarbeswarbhol@users.noreply.github.com>

--- users/views.py
+++ users/views.py
@@ -5,6 +5,21 @@ from django.contrib import messages
 
 
 def signup_view(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+     
+        # print(f"Username: {username}, Email: {email}, Password: {password}")
+        
+        User.objects.create(
+            username=username,
+            email=email,
+            password=password
+        )
+        messages.success(request, 'Account created successfully!')
+        return redirect('login')
+    
     return render(request, 'auth/signup.html')
 
 
